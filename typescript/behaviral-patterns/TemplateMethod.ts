5c54654b307a068135a2c7e0b5be31eb80a6cf21
mixnuts07 <french0719@gmail.com>
2023-09-12 22:51:33 +0900

    add Template Method

--- typescript/behaviral-patterns/TemplateMethod.ts
+++ typescript/behaviral-patterns/TemplateMethod.ts
@@ -1,8 +1,9 @@
 // Template Method (テンプレートメソッド): アルゴリズムの骨格を定義し、ステップの実装をサブクラスに委譲。
-// ...
+// ... 一連のステップの骨格を定義する方法を提供するが、そのステップの実装はサブクラスにまかせるパターン。
+// ... アルゴリズムの構造は同じままで、特定のステップの詳細を変更できる。
+// ... コードの重複を避け、拡張性を向上させる場合に有用
 // ... 具体的には、
 // 使用例
-// ...
 // ...
 // ...
 // ...
@@ -11,3 +12,53 @@
 // ...
 // ...
 // ...
+
+// Abstract Class
+abstract class RecipeTemplate {
+  // Template Method
+  prepareRecipe(): void {
+    this.boilWater();
+    this.brew();
+    this.pourInCup();
+    this.addCondiments();
+  }
+
+  boilWater(): void {
+    console.log("Boiling Water");
+  }
+
+  abstract brew(): void;
+  abstract addCondiments(): void;
+
+  pourInCup(): void {
+    console.log("Pouring into Cup");
+  }
+}
+
+// Concrete Class
+class TeaRecipe extends RecipeTemplate {
+  brew(): void {
+    console.log("Steeping the tea");
+  }
+
+  addCondiments(): void {
+    console.log("Adding Lemon");
+  }
+}
+
+class CoffeeRecipe extends RecipeTemplate {
+  brew(): void {
+    console.log("Dripping Coffee Through filter");
+  }
+
+  addCondiments(): void {
+    console.log("Adding Sugar And Milk");
+  }
+}
+
+// Client Code
+const tea = new TeaRecipe();
+tea.prepareRecipe();
+
+const coffee = new CoffeeRecipe();
+coffee.prepareRecipe();
